684cae85b291719ec607b4483cd28e935d3c1e62
Ahmed Sherif <45398293+AhmedSherif4@users.noreply.github.com>
2023-02-03 16:01:37 +0200

    Create arrow function.dart

---
+++ where_map_foreach/arrow function.dart
@@ -0,0 +1,32 @@
+void main(List<String> args) {
+// Write an arrow function that takes two integers as input and returns their sum.
+  int a = 20;
+  int b = 30;
+// output should be 50
+
+
+// Write an arrow function that takes a string as input and returns its length.
+  String s = 'Proud Of Yourself';
+// output should be 17
+
+
+// Write an arrow function that takes two strings as input and returns true if they are equal and false otherwise.
+  String s1 = 'is it true?';
+  String s2 = 'is it false?';
+// output should be false
+
+
+// Write an arrow function that takes an array of integers as input and returns the sum of all elements.
+  List<int> numbers = [2, 2, 2, 2, 4, 4, 4];
+// output should be 20
+
+// Write an arrow function that takes a list of numbers and returns the largest number in the list.
+  List<int> listOfNumbers = [5, 7, 10, 1, 0, -2, 22];
+// output should be 22
+
+
+// Write an arrow function that takes a list of strings and returns a list of only the strings that have a length greater than 5.
+  List<String> strings = ['Hany', 'Ahmed', 'Yousif', 'Amr', 'Asmaa', 'Sarah'];
+// output should be [Ahmed, Yousif, Asmaa, Sarah]
+
+}
